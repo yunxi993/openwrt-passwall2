alecthw/openwrt-passwall

luci: fix firewall `update_wan_sts` function

luci-app-passwall/root/usr/share/passwall/iptables.sh
@@ -795,6 +795,8 @@ filter_direct_node_list() {
 update_wan_sets() {
 	local log=$1
 
+	[ -z "$(command -v get_wan_ips)" ] && . "$UTILS_PATH"
+
 	local WAN_IP=$(get_wan_ips ip4)
 	[ -n "$WAN_IP" ] && {
 		ipset -F "$IPSET_WAN"
@@ -1487,7 +1489,7 @@ start() {
 }
 
 stop() {
-	[ -z "$(command -v echolog)" ] && . /usr/share/passwall/utils.sh
+	[ -z "$(command -v echolog)" ] && . "$UTILS_PATH"
 	del_firewall_rule
 	[ $(config_t_get global flush_set_on_reboot "0") = "1" -o $(config_t_get global flush_set "0") = "1" ] && {
 		uci -q delete ${CONFIG}.@global[0].flush_set

luci-app-passwall/root/usr/share/passwall/nftables.sh
@@ -861,6 +861,8 @@ mwan3_start() {
 update_wan_sets() {
 	local log=$1
 
+	[ -z "$(command -v get_wan_ips)" ] && . "$UTILS_PATH"
+
 	local WAN_IP=$(get_wan_ips ip4)
 	[ -n "$WAN_IP" ] && {
 		nft flush set $NFTABLE_NAME $NFTSET_WAN
@@ -1502,7 +1504,7 @@ start() {
 }
 
 stop() {
-	[ -z "$(command -v echolog)" ] && . /usr/share/passwall/utils.sh
+	[ -z "$(command -v echolog)" ] && . "$UTILS_PATH"
 	del_firewall_rule
 	[ $(config_t_get global flush_set_on_reboot "0") = "1" -o $(config_t_get global flush_set "0") = "1" ] && {
 		uci -q delete ${CONFIG}.@global[0].flush_set

yunxi993/openwrt-passwall2

luci-app-passwall2/root/usr/share/passwall2/iptables.sh
@@ -618,6 +618,8 @@ filter_direct_node_list() {
 }
 
 update_wan_sets() {
+	[ -z "$(command -v get_wan_ips)" ] && . "$UTILS_PATH"
+
 	local WAN_IP=$(get_wan_ips ip4)
 	[ -n "$WAN_IP" ] && {
 		ipset -F "$IPSET_WAN"
@@ -1105,7 +1107,7 @@ start() {
 }
 
 stop() {
-	[ -z "$(command -v log_i18n)" ] && . /usr/share/passwall2/utils.sh
+	[ -z "$(command -v log_i18n)" ] && . "$UTILS_PATH"
 	del_firewall_rule
 	destroy_ipset $IPSET_PROXY_LAN
 	destroy_ipset $IPSET_PROXY_LAN6

luci-app-passwall2/root/usr/share/passwall2/nftables.sh
@@ -681,6 +681,8 @@ mwan3_start() {
 }
 
 update_wan_sets() {
+	[ -z "$(command -v get_wan_ips)" ] && . "$UTILS_PATH"
+
 	local WAN_IP=$(get_wan_ips ip4)
 	[ -n "$WAN_IP" ] && {
 		nft flush set $NFTABLE_NAME $NFTSET_WAN
@@ -1116,7 +1118,7 @@ start() {
 }
 
 stop() {
-	[ -z "$(command -v log_i18n)" ] && . /usr/share/passwall2/utils.sh
+	[ -z "$(command -v log_i18n)" ] && . "$UTILS_PATH"
 	del_firewall_rule
 	destroy_nftset $NFTSET_PROXY_LAN
 	destroy_nftset $NFTSET_PROXY_LAN6