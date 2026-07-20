45245705dd834e565b1fa04ec596ec4ca01785d0
A-BO <86697442+lwb1978@users.noreply.github.com>
2026-07-20 11:06:45 +0800

    luci: rename xray `network` to `method`

--- luci-app-passwall2/luasrc/passwall2/util_xray.lua
+++ luci-app-passwall2/luasrc/passwall2/util_xray.lua
@@ -178,7 +178,7 @@ function gen_outbound(flag, node, tag, proxy_table)
 						MaxConcurrentTry = 4
 					} or nil
 				},
-				network = node.transport,
+				[(api.compare_versions(xray_version, "<", "26.7.11")) and "network" or "method"] = node.transport, -- Todo: Remove version check and "network"
 				security = node.stream_security,
 				tlsSettings = (node.stream_security == "tls") and {
 					serverName = node.tls_serverName,
@@ -665,7 +665,7 @@ function gen_config_server(node)
 				protocol = node.protocol,
 				settings = settings,
 				streamSettings = {
-					network = node.transport,
+					[(api.compare_versions(xray_version, "<", "26.7.11")) and "network" or "method"] = node.transport, -- Todo: Remove version check and "network"
 					security = "none",
 					tlsSettings = ("1" == node.tls) and {
 						disableSystemRoot = false,
