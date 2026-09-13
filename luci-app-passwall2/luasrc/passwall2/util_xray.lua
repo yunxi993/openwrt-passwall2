2cc8cbce9e42bda38dde61f98210ad7373cf8297
Sil <59167936+yunxi993@users.noreply.github.com>
2026-09-13 20:42:22 +0800

    luci: Some custom adjust.

--- luci-app-passwall2/luasrc/passwall2/util_xray.lua
+++ luci-app-passwall2/luasrc/passwall2/util_xray.lua
@@ -493,7 +493,7 @@ function gen_outbound(flag, node, tag, proxy_table)
 				port = config_port,
 				domains = {"full:" .. node.address},
 				finalQuery = true,
-				disableCache = false,
+				disableCache = true,
 				serveStale = true,
 			}
 		end
@@ -1558,7 +1558,7 @@ function gen_config(var)
 	dns = {
 		tag = "dns-global",
 		hosts = {},
-		disableCache = (dns_cache and dns_cache == "0") and true or false,
+		disableCache = true,
 		disableFallback = true,
 		disableFallbackIfMatch = true,
 		servers = {},
@@ -1731,7 +1731,7 @@ function gen_config(var)
 					address = "localhost",
 					domains = domain,
 					finalQuery = true,
-					disableCache = false,
+					disableCache = true,
 					serveStale = true,
 				})
 			end
